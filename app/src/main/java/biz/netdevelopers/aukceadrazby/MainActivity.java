2b89638b526e56bc373ba84c7c175dc443abbc8f
Jindřich Urbánek <m3up4x@gmail.com>
2014-10-24 15:51:05 +0200

    test konfliktu

--- app/src/main/java/biz/netdevelopers/aukceadrazby/MainActivity.java
+++ app/src/main/java/biz/netdevelopers/aukceadrazby/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 public class MainActivity extends Activity {
 
     // hhh
-    // 999
+    // 777
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
