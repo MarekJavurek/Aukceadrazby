649b85e08f0a7a358686391a611918f8210bd44a
Jindřich Urbánek <m3up4x@gmail.com>
2014-11-03 22:55:57 +0100

    strasne sem s tim bojoval ale nakonec neprisel na to jak to udelat po svem a jen jsem skopiroval tvuj kod zbytek snad dodelam

--- app/src/main/java/biz/netdevelopers/aukceadrazby/JindraUkol4.java
+++ app/src/main/java/biz/netdevelopers/aukceadrazby/JindraUkol4.java
@@ -2,10 +2,15 @@ package biz.netdevelopers.aukceadrazby;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +19,8 @@ import java.util.List;
 public class JindraUkol4 extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jindra_ukol4);
 
@@ -28,9 +34,57 @@ public class JindraUkol4 extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner sItems = (Spinner) findViewById(R.id.JindraActivitySpinerPocetdni);
         sItems.setAdapter(adapter);
-
-
     }
+
+    // odeslani formulare
+    public void ClickJindraUkol4odeslat(View view) {
+        validujFormular();
+    }
+
+    // validace formulare
+    private boolean validujFormular() {
+        EditText jmeno = (EditText) findViewById(R.id.EditText_jindraUkol4_jmenoPrijmeni);
+        EditText email = (EditText) findViewById(R.id.EditText_jindraUkol4_Email);
+        EditText telefon = (EditText) findViewById(R.id.EditText_jindraUkol4_Telefon);
+
+        if (!isValidName(jmeno.getText())) {
+            jmeno.setError("Jméno je povinné");
+            jmeno.requestFocus();
+            return false;
+        } else if (!isValidPhone(telefon.getText())) {
+            telefon.setError("Telefon je vy špatném formátu");
+            telefon.requestFocus();
+            return false;
+        } else if (!isValidEmail(email.getText())) {
+            email.setError("Email je vy špatném formátu");
+            email.requestFocus();
+            return false;
+        } else {
+            email.setError(null);
+            telefon.setError(null);
+            jmeno.setError(null);
+            Toast.makeText(this, "Validace OK", Toast.LENGTH_LONG).show();
+            return true;
+        }
+    }
+
+    // fce na validaci emailu
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    // fce na validaci telefonu
+    public final static boolean isValidPhone(CharSequence target) {
+        return !TextUtils.isEmpty(target) && Patterns.PHONE.matcher(target).matches();
+    }
+
+    // fce na validaci jmena
+    public final static boolean isValidName(CharSequence target) {
+        return !TextUtils.isEmpty(target);
+    }
+
+
+
 
 
     @Override
