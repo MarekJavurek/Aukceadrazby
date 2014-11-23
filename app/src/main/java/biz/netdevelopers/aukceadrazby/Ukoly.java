ac387af14e3e41a12e357de2480c8fdc3f7a3d84
Jindřich Urbánek <m3up4x@gmail.com>
2014-11-23 14:28:55 +0100

    ...

--- app/src/main/java/biz/netdevelopers/aukceadrazby/Ukoly.java
+++ app/src/main/java/biz/netdevelopers/aukceadrazby/Ukoly.java
@@ -16,7 +16,6 @@ public class Ukoly extends Activity {
         setContentView(R.layout.activity_ukoly);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
